9e21c369c60906b7cc91543acf9684e803d05a25
krenvp <karenperarnau@gmail.com>
2023-07-28 10:25:14 -0400

    Init files

---
+++ src/pages/App.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const App = () => {
+  return <h1>Countdown timer</h1>;
+};
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/index.jsx"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+    publicPath: "./",
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+    alias: {
+      "@fonts": path.resolve(__dirname, "src/assets/fonts/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@context": path.resolve(__dirname, "src/context/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs|js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Countdown timer",
+      template: "./public/index.html",
+      inject: true,
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "build"),
+    port: 3000,
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+  },
+  stats: { errorDetails: true },
+};
